cda807871fdc028fadc9604f222fa41af1131452
Rajesh Kumar <rajesh@checkout51.com>
2013-11-21 22:03:34 -0800

    Finish unit tests for node-memqueue

--- memqueue.js
+++ memqueue.js
@@ -1,18 +1,21 @@
 "use strict";
-var Memcached = require('memcached');
-var sleep = require('sleep');
-var assert = require('assert');
+var memcached = require('memcached'),
+	memc = new memcached('localhost:11211'),
+	sleep = require('sleep'),
+	assert = require('assert'),
+	util = require('util'),
+	atom = require('atom-js'),
+	a = new atom()
+;
 
-function memq (q_name) {
-	var memc,
-		start_key = 'start_' + q_name,
+function memq(q_name) {
+	var start_key = 'start_' + q_name,
 		end_key = 'end_' + q_name,
 		lock_key = 'lock_' + q_name,
 		item_key_format = 'item_' + q_name + '_%d';
 
-	var memc = new Memcached('localhost:11211', {poolSize: 1000});
-	memc.add(start_key, 0);
-	memc.add(end_key, 0);
+	memc.set(start_key, 0, 0, function() {});
+	memc.set(end_key, 0, 0, function() {});
 
 	var _unlock = function(cb) {
 		memc.del(lock_key, cb);
@@ -25,9 +28,13 @@ function memq (q_name) {
 
 	this.push = function(elem, cb) {
 		_lock(function() {
-				memc.incr(end_key, i, function(err, i) {
-						memc.set(util.format(item_key_format, i), $elem, function() {
-								_unlock(cb(elem));
+				memc.incr(end_key, 1, function(err, i) {
+						memc.set(util.format(item_key_format, i), elem, 0, function() {
+								_unlock(function() {
+										if (cb) {
+											cb(elem);
+										}
+									});
 							});
 					});
 			});
@@ -35,13 +42,15 @@ function memq (q_name) {
 
 	this.pop = function(cb) {
 		_lock(function() {
-				memc.incr(start_key, function(err, i) {
+				memc.incr(start_key, 1, function(err, i) {
 						memc.get(util.format(item_key_format, i), function(err, elem) {
 								var unlock_cb = function() {
 									cb(elem);
 								}
-								if (false === elem) { // eoq
-									memc.incr(end_key, unlock_cb);
+								if (elem === false) { // eoq
+									memc.incr(end_key, 1, function(err) {
+											unlock_cb();
+										});
 								} else {
 									unlock_cb();
 								}
@@ -50,9 +59,11 @@ function memq (q_name) {
 			});
 	};
 
-	this.peek = function() {
+	this.peek = function(cb) {
 		memc.get(start_key, function(err, i) {
-				memc.get(util.format(item_key_format, i+1), cb);
+				memc.get(util.format(item_key_format, i+1), function(err, elem) {
+						cb(elem);
+					});
 			});
 	}
 
@@ -77,45 +88,162 @@ function memq (q_name) {
 	// delete the entire queue
 	this.del = function(cb) {
 		_lock(function() {
-				memc.get(this.start_key, function(err, start) {
-						memc.get(this.end_key, function(err, end) {
-								var memc_delete = function(key, i) {
-									if (i < end) {
-										memc.del(util.format(this.item_key_format, i+1), function() {
-												memc_delete(i+1);
-											});
-									} else {
-										memc.set(start_key, end, function() {
-												_unlock(cb);
-											});
-									}
-								}
-								memc_delete(start);
-							});
+				memc.getMulti([start_key, end_key], function(err, results) {
+						var memc_delete = function(i) {
+							if (i < results[end_key]) {
+								memc.del(util.format(item_key_format, i+1), function() {
+										memc_delete(i+1);
+									});
+							} else {
+								memc.set(start_key, results[end_key], 0, function() {
+										_unlock(cb);
+									});
+							}
+						}
+						memc_delete(results[start_key]);
 					});
 			});
 	};
-
-	this.end = function(cb) {
-		memc.end();
-		if (cb) {
-			cb();
-		}
-	}
-
 };
 
 // unit tests
-var q = new memq('testq');
-q.count(function(count) {
-		assert.equal(count, 0);
-		q.is_empty(function(ret) {
-				assert.equal(ret, true);
-				q.push('a');
-				q.push('b');
-				q.push('c');
-				q.end();
-			});
+var q1 = new memq('testq1');
+var q2 = new memq('testq2');
+var q3 = new memq('testq3');
 
-	});
-
+a.chain(
+		function(next) {
+			q1.count(function(count) {
+					assert.equal(count, 0);
+					next();
+				});
+		},
+		function(next) {
+			q1.is_empty(function(ret) {
+					assert.equal(ret, true);
+					next();
+				});
+		},
+		function(next) {
+			var elemsToPush = ['a', 'b', 'c'];
+			var pushFunc = function(elem, cb) {
+				if (elem) {
+					q1.push(elem, function() {
+							return pushFunc(elemsToPush.shift(), cb)
+						});
+				} else {
+					return cb();
+				}
+			};
+			pushFunc(elemsToPush.shift(), next);
+		},
+		function(next) {
+			q1.count(function(count) {
+					assert.equal(count, 3);
+					next();
+				});
+		},
+		function(next) {
+			q1.is_empty(function(ret) {
+					assert.equal(ret, false);
+					next();
+				});
+		},
+		function(next) {
+			q1.peek(function(elem) {
+					assert.equal(elem, 'a');
+					next();
+				});
+		},
+		function(next) {
+			q1.pop(function(elem) {
+					assert.equal(elem, 'a');
+					next();
+				});
+		},
+		function(next) {
+			q1.count(function(count) {
+					assert.equal(count, 2);
+					next();
+				});
+		},
+		function(next) {
+			q1.pop(function(elem) {
+					assert.equal(elem, 'b');
+					next();
+				});
+		},
+		function(next) {
+			q1.push('d', next);
+		},
+		function(next) {
+			q2.push('a2', next);
+		},
+		function(next) {
+			q1.pop(function(elem) {
+					assert.equal(elem, 'c');
+					next();
+				});
+		},
+		function(next) {
+			q1.peek(function(elem) {
+					assert.equal(elem, 'd');
+					next();
+				});
+		},
+		function(next) {
+			q1.pop(function(elem) {
+					assert.equal(elem, 'd');
+					next();
+				});
+		},
+		function(next) {
+			q1.pop(function(elem) {
+					assert.equal(elem, false);
+					next();
+				});
+		},
+		function(next) {
+			q1.peek(function(elem) {
+					assert.equal(elem, false);
+					next();
+				});
+		},
+		function(next) {
+			q2.pop(function(elem) {
+					assert.equal(elem, 'a2');
+					next();
+				});
+		},
+		function(next) {
+			var elemsToPush = ['a', 'b', 'c'];
+			var pushFunc = function(elem, cb) {
+				if (elem) {
+					q3.push(elem, function() {
+							return pushFunc(elemsToPush.shift(), cb)
+						});
+				} else {
+					return cb();
+				}
+			};
+			pushFunc(elemsToPush.shift(), next);
+		},
+		function(next) {
+			q3.count(function(count) {
+					assert.equal(count, 3);
+					next();
+				});
+		},
+		function(next) {
+			q3.del(function() {
+					q3.count(function(count) {
+							assert.equal(count, 0);
+							next();
+						});
+				});
+		},
+		function(next) {
+			memc.end();
+			next();
+		}
+);
